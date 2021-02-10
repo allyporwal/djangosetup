e159a7f3742550fa30f4beb98dd0a21b72320c45
allyporwal <allyporwal@gmail.com>
2021-02-10 09:05:27 +0000

    Fix settings.py again

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SEVRET_KEY', 'jd7hz9497ryxdl*52orb()s+uzf*rhrgbblf+5f=67!_+s=a)z')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'jd7hz9497ryxdl*52orb()s+uzf*rhrgbblf+5f=67!_+s=a)z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
